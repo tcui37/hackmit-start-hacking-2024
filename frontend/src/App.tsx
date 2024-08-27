81708b53be9860d3841a9c9a7e9bfdc85cd247a1
Tony Cui <tacui@mit.edu>
2024-08-27 14:07:28 -0700

    yea

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
-
+import Child
+ from './components/Child';
 function App() {
   const [message, setMessage] = useState('');
+  const [clicks, setClicks] = useState(0);
 
   const fetchData = async () => {
     const response = await fetch('http://127.0.0.1:5000/');
@@ -13,9 +15,14 @@ function App() {
     fetchData();
   }, []);
 
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
   return (
     <div className="App">
       <h1> 
+        {<Child clicks={clicks} updateClick={handleClick} />}
         {message}
         </h1>
     </div>
